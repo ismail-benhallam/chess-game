81a03a74184481b788e09eebcbe38129ff12681b
Benhallam Ismaïl <ibenhallam@sqli.com>
2021-03-01 15:48:29 +0100

    Add "sayHello" method

--- src/com/sqli/Main.java
+++ src/com/sqli/Main.java
@@ -4,4 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
     }
+
+    public void sayHello(String name) {
+        System.out.println(String.format("Hello %s", name));
+    }
 }
